edfd66f944f8be05b4e96c409d8052d29d10a87b
iewher <goshagang@mail.ru>
2023-12-28 02:27:18 +0500

    finished template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,37 @@ class HomeController extends AbstractController
 
     public function index(): Response
     {
-        return $this->render("home/index.html.twig", [
-            'welcomeMessage' => 'Добро пожаловать в DOD_Case'
-        ]);
+        $posters = [
+            'poster1' => 'images/posters/poster1.jpg',
+            'poster2' => 'images/posters/poster2.jpg',
+            'poster3' => 'images/posters/poster3.jpg',
+            'poster4' => 'images/posters/poster4.jpg',
+        ];
+
+        $cards = [
+            'nike_zoom' => [
+                'title' => 'Nike Zoomx Vaporfly Next',
+                'price' => 31990,
+                'image' => 'images/cards/nikezoom.jpg',
+            ],
+            'jordan' => [
+                'title' => 'Jordan 1 Retro High Utility SP',
+                'price' => 24990,
+                'image' => 'images/cards/jordan.jpg',
+            ],
+            'nike_air' => [
+                'title' => 'Nike Air Max 97',
+                'price' => 19900,
+                'image' => 'images/cards/nikeair.jpg',
+            ],
+            'diadora' => [
+                'title' => 'Diadora N9000 Rally Giulietta',
+                'price' => 26790,
+                'image' => 'images/cards/diadora.jpg',
+            ],
+
+        ];
+
+        return $this->render("home/index.html.twig", ["cards" => $cards, "posters" => $posters]);
     }
 }
